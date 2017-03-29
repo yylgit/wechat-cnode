f923a63e4f2e93d0b265c8b6317850a7b90b412b
杨益良 <yangyiliang@meituan.com>
2017-03-29 17:42:38 +0800

    首页

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,15 +1,25 @@
 //topics.js
 import * as services from '../../services/services.js'
 var util =  require('../../utils/util');
+// 引入HtmlParser
+const HtmlParser = require('../../html-view/index')
 Page({
   data: {
     articalId: '5433d5e4e737cbe96dcef312',
-    artical: {}
+    artical: {},
+    html: null
   },
   onLoad: function (options) {
     let {articalId} = options;
     services.getArtical(articalId).then(res=>{
-      this.artical = res.data.data;
+      console.log(res.data.data)
+      // 解析HTML字符串
+      const html = new HtmlParser(res.data.data.content).nodes
+      this.setData({
+        artical: res.data.data,
+        html
+      }) 
+      
     })
   }
 })
--- pages/topics/topics.js
+++ pages/topics/topics.js
@@ -6,23 +6,43 @@ Page({
   data: {
     topics: [],
     pageIndex: 1,
-    tab: '', //ask share job good
-    limit: 10
+    activeTab: 'all', //ask share job good
+    limit: 20,
+    tabList: [{key: 'all', value: '全部'},
+    {key: 'good', value: '精华'},
+    {key: 'share', value: '分享'},
+    {key: 'ask', value: '问答'},
+    {key: 'job', value: '招聘'}],
+    fetchMore: false,
+    nodata: false
+  },
+  plainData: {
+    pullTimeStamp: Date.now()
   },
   onLoad: function () {
-    let {pageIndex,tab,limit} = this.data;
-    services.getTopics({
+    this.fetchList(true)
+  },
+  fetchList (reset) {
+    let {pageIndex,activeTab,limit} = this.data;
+    return services.getTopics({
       page: pageIndex,
-      tab: tab,
+      tab: activeTab,
       limit: limit
     }).then(res=>{
       res.data.data = res.data.data.map(item=>{
         item.create_at = util.formatTime(new Date(item.create_at))
         return item
       })
-      this.setData({
-        topics: res.data.data
-      })
+      if(reset) {
+        this.setData({
+          topics: res.data.data
+        })
+      } else {
+        this.setData({
+          topics: this.data.topics.concat(res.data.data)
+        })
+      }
+       
     })
   },
   itemClick (e) {
@@ -30,5 +50,55 @@ Page({
     wxNavigateTo("../detail/detail",{articalId})
     .then(()=>{})
     .catch(()=>{})
+  },
+  
+  onPullDownRefresh(e) {
+    this.plainData.pullTimeStamp = Date.now()
+    this.fetchList(true).then(()=>{
+      let time = Date.now()
+      let delayTime = Date.now() - this.plainData.pullTimeStamp;
+      let leftTime = 1000 - delayTime;
+      if(leftTime > 0) {
+          setTimeout(()=>{
+            wx.stopPullDownRefresh();
+          },leftTime)
+      }
+    })
+  },
+  onReachBottom: function () {  
+    this.setData({
+      fetchMore: true
+    })
+    this.data.pageIndex++;
+    this.fetchList(false).finally(()=>{
+      this.setData({
+        fetchMore: false
+      })
+    })
+     
+  },
+  onShareAppMessage: function () {
+    return {
+      title: 'CNODE社区小程序',
+      path: 'pages/topics/topics',
+      success: function(res) {
+        // 分享成功
+      },
+      fail: function(res) {
+        // 分享失败
+      }
+    }
+  },
+  tabClick(e) {
+    wx.showLoading({title: '正在加载'})
+    this.setData({
+      activeTab: e.currentTarget.dataset.tab
+    })
+    this.fetchList(true).finally(()=>{
+       wx.hideLoading()
+    })
+  },
+  toLoginClick() {
+    
   }
 })

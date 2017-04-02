d09a1bd038e9a41c1e133c6f0c3ee35761d6b30a
杨益良 <yangyiliang@meituan.com>
2017-04-02 22:36:01 +0800

    发表帖子

--- pages/newTopic/newTopic.js
+++ pages/newTopic/newTopic.js
@@ -1,3 +1,5 @@
+
+import {wxGetSystemInfo} from '../../utils/wxApi';
 Page({
     data: {
         tab: [{
@@ -7,12 +9,24 @@ Page({
         },{
             key: 'job', value: '招聘'
         }],
-        index: -1
+        index: -1,
+        textareaHeight: '100rpx'
     },
     bindPickerChange: function(e) {
         console.log('picker发送选择改变，携带值为', e.detail.value)
         this.setData({
             index: e.detail.value
+        })
+    },
+    onLoad () {
+        wxGetSystemInfo()().then(data=>{
+            console.log(data)
+            let {screenHeight,pixelRatio} = data;
+            let rpxHeight = screenHeight * pixelRatio;
+            let textareaHeight = rpxHeight - (30+80+80+120+40);
+            this.setData({
+                textareaHeight: textareaHeight + 'rpx'
+            })
         })
     }
 })
